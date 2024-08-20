866bf16786140e52172d063a8f5a496dda1cc820
Johan Binst <johan.binst@gmail.com>
2024-08-20 21:17:38 +1100

    [BUILD] project: Init repo

---
+++ src/main/java/json/spring_assistant/SpringAssistantApplication.java
@@ -0,0 +1,13 @@
+package json.spring_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/json/spring_assistant/SpringAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package json.spring_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
